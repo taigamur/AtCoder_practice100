21a13d90000309cb6dbab56ba2081f09ded9517c
Taiga Miura <miura.t84@gmail.com>
2021-09-14 09:48:23 +0900

    0916

--- 1.cpp
+++ 1.cpp
@@ -1,33 +1,23 @@
-/*全探索　全列挙型
-
-0728 ok
-
-*/
-
 #include <iostream>
 using namespace std;
 
 int main(){
-
     int n,x;
     while(true){
         cin >> n >> x;
-        if(n==0 && x==0){
-            return 0;
+        if(n == 0 && x == 0){
+            break;
         }
-
         int sum = 0;
-        for(int i=1;i<=n;i++){
-            for(int j=i+1;j<=n;j++){
-                int k = x - i - j;
-                if(k > j && k <= n){
+        for(int i = 1; i <= n; i++){
+            for(int j = i + 1; j <= n; j++){
+                int z = x - i - j;
+                if(z <= n && z > j){
                     sum++;
                 }
             }
         }
-
         cout << sum << endl;
-
-
     }
+
 }
